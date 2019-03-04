02758fc94aa89e533b92d156252b2016b0c15304
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-03-04 11:36:02 -0500

    4.Estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,28 +1,33 @@
 import React from 'react';
 
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+            alt="Avatar"
+          />
           <h1>
             Richard <br /> Kaufman
           </h1>
         </div>
 
-        <div>
-          <p>Frontend Engineer</p>
-          <p>@sparragus</p>
+        <div className="Badge__section-info">
+          <h3>Frontend Engineer</h3>
+          <div>@sparragus</div>
         </div>
 
-        <div>#platziconf</div>
+        <div className="Badge__footer">#platziconf</div>
       </div>
     );
   }
