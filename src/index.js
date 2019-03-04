ba74f3a2d181862afce29300acd82233e3cd04d9
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-03-04 10:05:33 -0500

    1. ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi Badges!';
+
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const element = <h1>Hello, Platzi Badges!</h1>;
+
 const container = document.getElementById('app');
 
-const element = <h1>Hello, Platzi Badges</h1>;
+// ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(element, container);
