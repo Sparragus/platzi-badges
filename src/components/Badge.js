b34b523f5f4b1ab4e47e81ad96f5dabf0a9eabe7
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-03-04 11:49:10 -0500

    5.Props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -14,17 +14,17 @@ class Badge extends React.Component {
         <div className="Badge__section-name">
           <img
             className="Badge__avatar"
-            src="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+            src={this.props.avatarUrl}
             alt="Avatar"
           />
           <h1>
-            Richard <br /> Kaufman
+            {this.props.firstName} <br /> {this.props.lastName}
           </h1>
         </div>
 
         <div className="Badge__section-info">
-          <h3>Frontend Engineer</h3>
-          <div>@sparragus</div>
+          <h3>{this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
         </div>
 
         <div className="Badge__footer">#platziconf</div>
--- src/index.js
+++ src/index.js
@@ -7,4 +7,13 @@ import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(
+  <Badge
+    firstName="Richard"
+    lastName="Kaufman"
+    avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+    jobTitle="Frontend Engineer"
+    twitter="sparragus"
+  />,
+  container
+);
