47a5a9360d9910871ec339e223275820a0c26f72
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-03-04 10:42:20 -0500

    2. JSX

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const jsx = <h1>Hello, Platzi Badges!</h1>;
+// const element = React.createElement(
+//   'a',
+//   { href: 'https://platzi.com' },
+//   'Ir a Platzi'
+// );
+// const name = 'Stephanie';
+// const sum = () => 3 + 3;
+// const element = React.createElement('h1', {}, `Hola, soy ${name}`);
+// const jsx = <h1>Hola soy, {undefined}</h1>;
 
+const jsx = (
+  <div>
+    <h1>Hola, soy Richard</h1>
+    <p>Soy ingeniero frontend.</p>
+  </div>
+);
+
+const element = React.createElement(
+  'div',
+  {},
+  React.createElement('h1', {}, 'Hola, soy Richard'),
+  React.createElement('p', {}, 'Soy ingeniero de la web.')
+);
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
