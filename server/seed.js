17d2f344dd47554346f6ad88062db41888af8410
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-03-03 19:41:30 -0500

    Initial version

---
+++ server/seed.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const faker = require('faker');
+const md5 = require('md5');
+
+function createBadges(limit = 50) {
+  const result = [];
+
+  for (let i = 0; i < limit; i++) {
+    const firstName = faker.name.firstName();
+    const lastName = faker.name.lastName();
+    const email = faker.internet.email();
+
+    result.push({
+      id: faker.random.uuid(),
+      firstName,
+      lastName,
+      email,
+      jobTitle: faker.name.jobTitle(),
+      twitter: `${firstName}${lastName}${faker.address.zipCode()}`,
+      avatarUrl: `https://www.gravatar.com/avatar/${md5(email)}?d=identicon`,
+    });
+  }
+
+  return result;
+}
+
+function main() {
+  const data = {
+    badges: createBadges(),
+  };
+
+  fs.writeFileSync(
+    path.resolve(__dirname, 'db.json'),
+    JSON.stringify(data, null, 4)
+  );
+}
+
+main();
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.getElementById('app');
+
+const element = <h1>Hello, Platzi Badges</h1>;
+ReactDOM.render(element, container);
