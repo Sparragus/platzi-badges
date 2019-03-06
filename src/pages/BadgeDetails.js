3faeb39963ae464e3473991bd939f955de810fa4
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-03-06 11:10:30 -0500

    22.Portals

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 
 import './styles/BadgeDetails.css';
@@ -50,6 +51,10 @@ function BadgeDetails(props) {
 
               <div>
                 <button className="btn btn-danger">Delete</button>
+                {ReactDOM.createPortal(
+                  <h1>Hola, realmente no estoy aquí</h1>,
+                  document.getElementById('modal')
+                )}
               </div>
             </div>
           </div>
