c46ff50d690dddaaac99aef0a0221fd7dc2061e2
Richard B. Kaufman-López <richardbkaufman@gmail.com>
2019-03-06 10:53:02 -0500

    21.UIContainerComponents

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Layout from './Layout';
 import Home from '../pages/Home';
 import Badges from '../pages/Badges';
 import BadgeNew from '../pages/BadgeNew';
-import BadgeDetails from '../pages/BadgeDetails';
+import BadgeDetails from '../pages/BadgeDetailsContainer';
 import BadgeEdit from '../pages/BadgeEdit';
 import NotFound from '../pages/NotFound';
 
---
+++ src/pages/BadgeDetailsContainer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import BadgeDetails from './BadgeDetails';
+import PageLoading from '../components/PageLoading';
+import PageError from '../components/PageError';
+import api from '../api';
+
+class BadgeDetailsContainer extends React.Component {
+  state = {
+    loading: true,
+    error: null,
+    data: undefined,
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const data = await api.badges.read(this.props.match.params.badgeId);
+      this.setState({ loading: false, data: data });
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
+
+  render() {
+    if (this.state.loading) {
+      return <PageLoading />;
+    }
+
+    if (this.state.error) {
+      return <PageError error={this.state.error} />;
+    }
+
+    return <BadgeDetails badge={this.state.data} />;
+  }
+}
+
+export default BadgeDetailsContainer;
