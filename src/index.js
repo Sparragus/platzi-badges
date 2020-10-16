fb3dd78855baf75099823531d165d27b9593c8f3
Jair Avilés <hi@jairaviles.mx>
2020-10-16 15:52:13 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 
 const container = document.getElementById('app');
 
-const element = <h1>Hello, Platzi Badges</h1>;
-ReactDOM.render(element, container);
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hello, Platzi Badges</h1>
+  </React.StrictMode>, 
+  container);
