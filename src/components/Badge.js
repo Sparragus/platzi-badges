2ea5d54abb4cea120bceb274c33091d05fa58b49
Jair Avilés <hi@jairaviles.mx>
2020-10-18 16:59:45 -0700

    Add CSS styles definition

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={ confLogo } alt="Logo de la conferencia" />
         </div>
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="avatar" />
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="avatar" />
           <h1> Jair <br/> Aviles</h1>
         </div>
-        <div>
-          <p>Fullstack Developer</p>
-          <p>@yajairo87</p>
+        <div className="Badge__section-info">
+          <h3>Fullstack Developer</h3>
+          <div>@yajairo87</div>
         </div>
-        <div>
-          Platzi conf
+        <div className="Badge__footer"> 
+          #platziconf
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
+import './global.css';
 import Badge from './components/Badge';
 
 const container = document.getElementById('app');
