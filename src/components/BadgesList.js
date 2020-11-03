2b572d33cb8ef81deb8ce0a97dced3b3c2cbb7d8
Jair Avilés <hi@jairaviles.mx>
2020-11-02 21:00:41 -0800

    Replace fixture with fake db seeds

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import BadgesListItem from '../components/BadgesListItem';
 
 class BadgesList extends Component {
   render() {
+
+    if (this.props.badges.length === 0) {
+      return (
+        <div>
+          <h3>No encontramos ningún badge</h3>
+          <Link className="btn btn-primary" to="/badges/new"></Link>
+        </div>
+      );
+    }
+
     return (
       <div className="BadgesList">
         <ul className="list-unstyled">
