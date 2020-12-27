c70062b67f58217ada2169e78d0571d3a941941c
EverSorto <evercuentatres@gmail.com>
2020-12-27 10:16:23 -0600

    uso de React.createElement y jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const container = document.getElementById('app');
+//const container = document.getElementById('app');
+//const element = <h1>Hola Mundo</h1>;
+//ReactDOM.render(element, container);
+//const jsx = <h1>Hello, platzi Badges!!</h1>
+//const element = React.createElement("a",{href:'https://wikipedia.com',},'Wikipedia') 
+const name = "Ever Alfredo"
+//const element = React.createElement('h1',{},`hola soy ${name}`)
+//const jsx = (<div>
+  //   <h1>hola soy {name}</h1>
+    // <p>Hola mundo desde Rect</p>
+//</div>)
 
-const element = <h1>Hello, Platzi Badges</h1>;
-ReactDOM.render(element, container);
+const element = React.createElement('div', {}, 
+    React.createElement('h1',{},`hola soy ${name}`),
+    React.createElement('p',{},"Estudio para seguridad informatica")
+)
+   
+const container = document.getElementById('app')
+ReactDOM.render(element,container)
+
+
+
