30dc4212a3182c61fe8573dfee7504b19b747488
jose Brusa <josebrusa@outlook.com>
2021-03-27 03:54:32 -0300

    Nuestra primera página

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import 'bootstrap/dist/css/bootstrap.css';
-
-// import './global.css';
-// import App from './components/App';
-
-// const container = document.getElementById('app');
-
-// ReactDOM.render(<App />, container);
-
-
 import React from 'react';
 
 import ReactDOM from 'react-dom';
 
-import Badge from './componets/Badge';
+// import Badge from './componets/Badge';
 import './global.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import BadgeNew from './componets/BadgeNew';
 
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badge 
-    firstName="Jose" 
-    lastName="Brusa" 
-    jobTitle="Frontend Engineer" 
-    twitter="brusa_jose" 
-    avatarUrl="https://s.gravatar.com/avatar/4133e43f063b9e916b24835664fa19e4?s=80"
-    />, 
-    container);
+ReactDOM.render(<BadgeNew />,  container);
 
