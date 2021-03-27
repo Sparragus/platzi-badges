2e6c7439b016630a5f367416144b8f549933caba
jose Brusa <josebrusa@outlook.com>
2021-03-27 02:08:13 -0300

    platzi init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
+
+// import './global.css';
+// import App from './components/App';
+
+// const container = document.getElementById('app');
+
+// ReactDOM.render(<App />, container);
+
+
 import React from 'react';
+
 import ReactDOM from 'react-dom';
+
+import Badge from './componets/Badge';
+import './global.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import './global.css';
-import App from './components/App';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(<Badge />, container);
+
--- src/pages/NotFound.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function NotFound() {
-  return <h1>404: Not Found</h1>;
-}
-
-export default NotFound;
