875b9d2a597bbb4075e3ecd8b90494910337df80
Carlos04 <cvargaslopez769@gmail.com>
2021-06-16 16:58:22 -0600

    add

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar' 
+import Home from '../pages/Home'
 
 function Layout(props) {
   return (
     <React.Fragment>
-      <Navbar /> 
+      {/* <Navbar />  */}
       {props.children}
     </React.Fragment>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import stars from '../images/stars.svg'
 import "./styles/Badge.css"
+import homeLogo from '../images/platziconf-logo.svg'
 
 function Home() {
   return (
     <React.Fragment>
-      <div className="Badges__home"></div>
+      <div className="Badges__home d-flex align-items-center justify-content-center">
+       <div className="container text-light ">
+          <img src={homeLogo} className="img-fluid" />
+          <h2 className="fw-bold  mt-3" style={{letterSpacing: "-1px"}}>PRINT YOUR BADGES</h2>
+          <p className="fw-bold">The easiest way to manage your <br/> conference</p>
+          <buttton className="btn btn-primary fw-bold">Start now</buttton>
+       </div>
+      </div>
     </React.Fragment>
     
   )
