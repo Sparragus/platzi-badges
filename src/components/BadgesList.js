8e1e86eeb2c4f49cf7d68353ebb7b1f463c306b0
Jair Avilés <hi@jairaviles.mx>
2021-07-05 22:02:04 -0700

    Add user filter feature

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,39 +1,106 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-import BadgesListItem from '../components/BadgesListItem';
+import './styles/BadgesList.css';
+import Gravatar from './Gravatar';
 
-class BadgesList extends Component {
+class BadgesListItem extends React.Component {
   render() {
-
-    const badgesList = this.props.badges.slice(0).reverse();
-
-    if (badgesList.length === 0) {
-      return (
-        <div>
-          <h3>No encontramos ningún badge</h3>
-          <Link className="btn btn-primary" to="/badges/new"></Link>
-        </div>
-      );
-    }
-
     return (
-      <div className="BadgesList">
-        <ul className="list-unstyled">
-          {badgesList.map(badge => {
-            return (
-              <li key={badge.id}>
-                <Link className="text-reset text-decoration-none"
-                      to={`/badges/${badge.id}`}>
-                  <BadgesListItem badge={badge} />
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+      <div className="BadgesListItem">
+        <Gravatar
+          className="BadgesListItem__avatar"
+          email={this.props.badge.email}
+        />
+
+        <div>
+          <strong>
+            {this.props.badge.firstName} {this.props.badge.lastName}
+          </strong>
+          <br />@{this.props.badge.twitter}
+          <br />
+          {this.props.badge.jobTitle}
+        </div>
       </div>
-    )
+    );
   }
+}
+
+function useSearchBadges(badges) {
+  const [query, setQuery] = React.useState('');
+  const [filteredBadges, setFilteredBadges] = React.useState(badges);
+
+  React.useMemo(() => {
+    const result = badges.filter(badge => {
+      return `${badge.firstName} ${badge.lastName}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+
+    setFilteredBadges(result);
+  }, [badges, query]);
+
+  return { query, setQuery, filteredBadges };
+}
+
+function BadgesList(props) {
+  const badges = props.badges;
+
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges);
+
+  if (filteredBadges.length === 0) {
+    return (
+      <div>
+        <div className="form-group mb-4">
+          <label>Filter Badges</label>
+          <input
+            type="text"
+            className="form-control"
+            value={query}
+            onChange={e => {
+              setQuery(e.target.value);
+            }}
+          />
+        </div>
+
+        <h3>No badges were found</h3>
+        <Link className="btn btn-primary" to="/badges/new">
+          Create new badge
+        </Link>
+      </div>
+    );
+  }
+
+  return (
+    <div className="BadgesList">
+      <div className="form-group mb-4">
+        <label>Filter Badges</label>
+        <input
+          type="text"
+          className="form-control"
+          value={query}
+          onChange={e => {
+            setQuery(e.target.value);
+          }}
+        />
+      </div>
+
+      <ul className="list-unstyled">
+        {filteredBadges.map(badge => {
+          return (
+            <li key={badge.id}>
+              <Link
+                className="text-reset text-decoration-none"
+                to={`/badges/${badge.id}`}
+              >
+                <BadgesListItem badge={badge} />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default BadgesList;
